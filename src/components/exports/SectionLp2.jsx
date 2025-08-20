0ff894faf077d8d79fef3a76ee17f83d112dc5c4
mircomania <mirco.rodriguez@hotmail.com>
2025-08-20 12:46:59 -0400

    4 commit mejoravit 1.2 - full landing page

--- src/components/exports/SectionLp2.jsx
+++ src/components/exports/SectionLp2.jsx
@@ -1,7 +1,15 @@
+import styles from '../../styles/modules/sectionLp2.module.css';
+
 export const SectionLp2 = () => {
     return (
-        <section>
-            <h2></h2>
+        <section className={styles.sectionContainer}>
+            <div className={styles.sectionContenido}>
+                <h2 className="light-text">
+                    El crédito Mejoravit permite obtener hasta <strong>$163,030.21</strong> pesos para remodelar o ampliar tu vivienda, dependiendo de
+                    tu plazo, capacidad de pago y saldo en la subcuenta de vivienda. Ofrece una tasa de interés anual del 11%, y es accesible con solo
+                    6 meses cotizando en el IMSS con el mismo patrón.
+                </h2>
+            </div>
         </section>
     );
 };
